c5103bb752a214f5948ce60df8828a3e6ad5371d
lifenjoiner <lifenjoiner@163.com>
2019-04-27 21:56:27 +0800

    use js base64 decode

--- ezx-pac_auto-pac.js
+++ ezx-pac_auto-pac.js
@@ -52,10 +52,77 @@ function get_html_text(url, dom_opr){
 }
 
 /*******/
-function debase64_file(file){
-    return $cmd$("base64 -d "+ file);
-}
+var Base64 = {
+	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
+	decode : function (input) {
+		var output = "";
+		var chr1, chr2, chr3;
+		var enc1, enc2, enc3, enc4;
+		var i = 0;
+
+		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+		while (i < input.length) {
+
+			enc1 = this._keyStr.indexOf(input.charAt(i++));
+			enc2 = this._keyStr.indexOf(input.charAt(i++));
+			enc3 = this._keyStr.indexOf(input.charAt(i++));
+			enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
+
+			output = output + String.fromCharCode(chr1);
+
+			if (enc3 != 64) {
+				output = output + String.fromCharCode(chr2);
+			}
+			if (enc4 != 64) {
+				output = output + String.fromCharCode(chr3);
+			}
+
+		}
+
+		output = Base64._utf8_decode(output);
+
+		return output;
+
+	},
+
+	_utf8_decode : function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+
+		while ( i < utftext.length ) {
+
+			c = utftext.charCodeAt(i);
+
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+
+		}
+
+		return string;
+	}
+};
+
+/*******/
 function textline2array(text) {
     text = text.replace(/\r\n/mg, "\n");
     text = text.replace(/\r/mg, "\n");
@@ -92,9 +159,10 @@ function get_gfw_result_str() {
     var text, tmp, result;
     var url;
     //
-    url = "https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt"; // 6h
+    url = "https://gitlab.com/gfwlist/gfwlist/raw/master/gfwlist.txt"; // 6h
     $msg(url);
-    text = get_html_text(url, "base64 -d");
+    text = get_html_text(url, "");
+    text = Base64.decode(text);
     $msg(text?"<got!":"<err?");
     //
     return text;
@@ -243,7 +311,12 @@ function clean_direct_hosts(items_p, items_b) {
 function update_result(){
     var gfw_list = get_gfw_result_items();
     //
-    data = data.replace("__RULES__", items2arraystr(gfw_list));
+    var result_str = items2arraystr(gfw_list);
+    if (result_str == '') {
+        $msg("Update gfwlist failed, abort updates!");
+        return;
+    }
+    data = data.replace("__RULES__", result_str);
     //
     var blacklist = get_blacklist_result_items();
     data = data.replace("__RULES_BLACKLIST__", items2arraystr(blacklist));
@@ -293,6 +366,6 @@ $cmd("cmd /c title auto-pac %time%");
 $msg("Set use pac yourself! Win10 only accepts online pac!");
 $msg(new Date() +" [Finished]");
 $msg("Sleeping hours: "+ sleep_h);
-WScript.Sleep(3600000*sleep_h);
+WScript.Sleep(3600000 * sleep_h);
 //in case of updates
 $run('cscript.exe //nologo "'+ WScript.ScriptFullName +'"', 6);
