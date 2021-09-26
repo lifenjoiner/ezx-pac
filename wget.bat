0ef60771c7b7967126346e4334a8921836846436
lifenjoiner <lifenjoiner@163.com>
2021-09-26 20:46:12 +0800

    echo msg to stderr

--- wget.bat
+++ wget.bat
@@ -4,13 +4,14 @@ setlocal ENABLEDELAYEDEXPANSION
 ::-code-::
 if "%~1"=="" goto :Exit
 :Lookup
-for /f "usebackq delims=" %%i in ("%~n0.lst") do (
+for /f "usebackq delims=" %%i in ("%~dpn0.lst") do (
+    echo %%i 1>&2
     if exist %%i (
         set app=%%i
     )
 )
 if "%app%"=="" (
-	title %~n0 path is not found, I'm repeating look for it in the %~n0.lst! :p
+	echo %~n0 path is not found, I'm repeating look for it in the %~n0.lst :p 1>&2
 	pause
 	goto :Lookup
 )
